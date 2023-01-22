121916556f07485f196f0c8ead5e8a25e68db0e4
Bmattholder <bmattholder@gmail.com>
2023-01-22 10:13:59 -0600

    minor fix

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCaOytvz-0pj77eI0drzr_Qqcrh9fcLkpA",
-  authDomain: "house-marketplace-app-49066.firebaseapp.com",
-  projectId: "house-marketplace-app-49066",
-  storageBucket: "house-marketplace-app-49066.appspot.com",
-  messagingSenderId: "722206594759",
-  appId: "1:722206594759:web:f7500d282fca84d3ef90e2"
+  apiKey: 'AIzaSyCaOytvz-0pj77eI0drzr_Qqcrh9fcLkpA',
+  authDomain: 'house-marketplace-app-49066.firebaseapp.com',
+  projectId: 'house-marketplace-app-49066',
+  storageBucket: 'house-marketplace-app-49066.appspot.com',
+  messagingSenderId: '722206594759',
+  appId: '1:722206594759:web:f7500d282fca84d3ef90e2',
 };
 
-export const db = getFirestore()
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
